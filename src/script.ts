fbbcceea5e194c3c1160497d9820ea5c438e40f1
OrhanMA <orhan.madi.assani@gmail.com>
2023-04-03 11:01:44 +0200

    Moved child classes methods to parent class

--- src/script.ts
+++ src/script.ts
@@ -1,63 +1,66 @@
-console.log("test");
+"use strict";
 
+const homepage = document.querySelector(".homepage") as HTMLDivElement;
+const game = document.querySelector(".game") as HTMLDivElement;
+
+const kamelFace = document.querySelector(".kamel-face") as HTMLImageElement;
+const billyFace = document.querySelector(".billy-face") as HTMLImageElement;
+
+const bars = document.querySelectorAll(
+  "progress"
+) as NodeListOf<HTMLProgressElement>;
+
+let kamelIsSelected: boolean = false;
+let billyIsSelected: boolean = false;
 export default class Streamer {
   name: string;
-  constructor(name) {
+  face: HTMLImageElement;
+  special: string;
+  specialNeed: HTMLParagraphElement;
+  specialFeed: HTMLButtonElement;
+  need: string;
+  currentNeed: HTMLParagraphElement;
+  currentFeed: HTMLButtonElement;
+  constructor(name: string) {
     name = this.name;
+  }
+  changePictureDisplay() {
+    this.face.classList.remove("hidden");
+  }
+  changeSpecialDisplay() {
+    this.specialNeed.textContent = this.special;
+    this.specialFeed.textContent = this.special;
+  }
+  changeFeed() {
+    this.currentNeed.textContent = this.need;
+    this.currentFeed.textContent = this.need;
+  }
+  setCharacter() {
+    this.changePictureDisplay();
+    this.changeSpecialDisplay();
+    this.changeFeed();
   }
 }
 
 class Billy extends Streamer {
-  food: string = "Donuts";
-
-  changeFoodDisplay() {
-    const need = document.querySelector(".need3") as HTMLParagraphElement;
-    const feed = document.querySelector(".feed3") as HTMLButtonElement;
-    need.textContent = this.food;
-    feed.textContent = this.food;
-  }
-  changePictureDisplay() {
-    billyFace.classList.remove("hidden");
-  }
-  changeFeed1() {
-    const feed1 = document.querySelector(".feed1") as HTMLButtonElement;
-    const need1 = document.querySelector(".need1") as HTMLParagraphElement;
-    feed1.textContent = "LOL";
-    need1.textContent = "League of Legends";
-  }
-  /* makeCharacterAppear() {
-    const billyFace = document.querySelector(".billy-face") as HTMLImageElement;
-    billyFace.classList.remove(".hidden");
-  } */
+  special: string = "Donuts";
+  face: HTMLImageElement = billyFace;
+  specialNeed = document.querySelector(".need3") as HTMLParagraphElement;
+  specialFeed = document.querySelector(".feed3") as HTMLButtonElement;
+  need: string = "LOL";
+  currentNeed = document.querySelector(".need1") as HTMLParagraphElement;
+  currentFeed = document.querySelector(".feed1") as HTMLButtonElement;
 }
 
 class Kamel extends Streamer {
-  activity: string = "Gaming";
-  changeActivityDisplay() {
-    const activity = document.querySelector(".need1") as HTMLParagraphElement;
-    const feed = document.querySelector(".feed1") as HTMLButtonElement;
-    activity.textContent = this.activity;
-    feed.textContent = this.activity;
-  }
-  changePictureDisplay() {
-    kamelFace.classList.remove("hidden");
-  }
-  changeFeed3() {
-    const feed3 = document.querySelector(".feed3") as HTMLButtonElement;
-    const need3 = document.querySelector(".need3") as HTMLParagraphElement;
-    feed3.textContent = "Tacos";
-    need3.textContent = "Tacos";
-  }
-  /* makeCharacterAppear() {
-    const kamelFace = document.querySelector(".kamel-face") as HTMLImageElement;
-    kamelFace.classList.remove(".hidden");
-  } */
+  special: string = "Gaming";
+  face: HTMLImageElement = kamelFace;
+  specialNeed = document.querySelector(".need1") as HTMLParagraphElement;
+  specialFeed = document.querySelector(".feed1") as HTMLButtonElement;
+  need: string = "Tacos";
+  currentNeed = document.querySelector(".need3") as HTMLParagraphElement;
+  currentFeed = document.querySelector(".feed3") as HTMLButtonElement;
 }
-const kamelFace = document.querySelector(".kamel-face") as HTMLImageElement;
-const billyFace = document.querySelector(".billy-face") as HTMLImageElement;
-
-let kamelIsSelected: boolean = false;
-let billyIsSelected: boolean = false;
 
 const billyChoice = document.querySelector(".billy-choice") as HTMLImageElement;
 billyChoice.addEventListener("click", () => {
@@ -65,7 +68,6 @@ billyChoice.addEventListener("click", () => {
   kamelIsSelected = false;
   billyChoice.classList.add("selected-border");
   kamelChoice.classList.remove("selected-border");
-  console.log(billyIsSelected, kamelIsSelected);
 });
 const kamelChoice = document.querySelector(".kamel-choice") as HTMLImageElement;
 kamelChoice.addEventListener("click", () => {
@@ -73,11 +75,7 @@ kamelChoice.addEventListener("click", () => {
   billyIsSelected = false;
   kamelChoice.classList.add("selected-border");
   billyChoice.classList.remove("selected-border");
-  console.log(billyIsSelected, kamelIsSelected);
 });
-
-const homepage = document.querySelector(".homepage") as HTMLDivElement;
-const game = document.querySelector(".game") as HTMLDivElement;
 
 const startButton = document.querySelector(
   ".start-button"
@@ -99,45 +97,29 @@ startButton.addEventListener("click", () => {
 function displayRightCharacter() {
   if (billyIsSelected === true && kamelIsSelected === false) {
     let billy = new Billy("Billy");
-    billy.changeFoodDisplay();
-    billy.changePictureDisplay();
-    billy.changeFeed1();
-    console.log(billy.food);
+    billy.setCharacter();
   } else if (billyIsSelected === false && kamelIsSelected === true) {
     let kamel = new Kamel("Kamel");
-    kamel.changeActivityDisplay();
-    kamel.changePictureDisplay();
-    kamel.changeFeed3();
-    console.log(kamel.activity);
+    kamel.setCharacter();
   }
 }
-const bars = document.querySelectorAll(
-  "progress"
-) as NodeListOf<HTMLProgressElement>;
 
 function startInterval() {
   let interval = setInterval(() => {
     bars.forEach((element) => {
       if (element.value <= 0) {
-        // console.log(`from 120 interval ID: ${interval}`);
         clearInterval(interval);
-        console.log("Game over!");
         gameOver();
       } else if (element.value > 50) {
         element.value = element.value - 10;
-        console.log(element.value);
       } else if (element.value > 15 && element.value <= 50) {
         element.value = element.value - 6;
-        console.log(element.value);
       } else if (element.value > 0 && element.value <= 20) {
         element.value = element.value - 2;
-        console.log(element.value);
       } else {
-        console.log("erreur?");
       }
     });
   }, 1500);
-  // console.log(`from line 137 interval ID:${interval}`);
 }
 
 function gameOver() {
